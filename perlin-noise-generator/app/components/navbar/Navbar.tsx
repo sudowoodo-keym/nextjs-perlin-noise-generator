d5d6db47208dac13b84ba4d9390058a012bda874
keym <20496472+sudowoodo-keym@users.noreply.github.com>
2026-02-05 10:43:41 -0500

    added hero

--- perlin-noise-generator/app/components/navbar/Navbar.tsx
+++ perlin-noise-generator/app/components/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 export const Navbar = () => {
   return (
-    <div className="w-full h-20 md:h-16 bg-neutral-700 backdrop-blur-xl fixed z-50"></div>
+    <div className="w-full h-20 md:h-16 bg-neutral-700 backdrop-blur-xl z-50"></div>
   )
 }
--- perlin-noise-generator/app/page.tsx
+++ perlin-noise-generator/app/page.tsx
@@ -3,8 +3,14 @@ import styles from "./homepage.module.css";
 import { Navbar } from "./components/navbar/Navbar";
 
 export default function Home() {
-  return (<div id={`${styles.pageWrap}`} className="min-w-full min-h-full">
+  return (
+    <div id={`${styles.pageWrap}`} className="min-w-full min-h-full">
       <Navbar />
+      <div id={`${styles.pageContainer}`} className="w-screen h-screen bg-neutral-800">
+        <div className={`${styles.hero} w-full h-svh text-center flex flex-col items-center justify-center`}>
+          <p className="text-3xl">Perlin Noise Generator</p>
+        </div>
+      </div>
     </div> 
   );
 }
