d3652d6b3984231c942724330ffacb2475339378
keym <20496472+sudowoodo-keym@users.noreply.github.com>
2026-02-05 00:01:08 -0500

    navbar and homepage tweaks

---
+++ perlin-noise-generator/app/components/navbar/Navbar.tsx
@@ -0,0 +1,5 @@
+export const Navbar = () => {
+  return (
+    <div className="w-full h-20 md:h-16 bg-neutral-700 backdrop-blur-xl fixed z-50"></div>
+  )
+}
--- perlin-noise-generator/app/page.tsx
+++ perlin-noise-generator/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import styles from "./homepage.module.css";
+import { Navbar } from "./components/navbar/Navbar";
 
 export default function Home() {
-  return (<div id={`${styles.pageWrap}`} className="min-w-full min-h-full"></div> 
+  return (<div id={`${styles.pageWrap}`} className="min-w-full min-h-full">
+      <Navbar />
+    </div> 
   );
 }
