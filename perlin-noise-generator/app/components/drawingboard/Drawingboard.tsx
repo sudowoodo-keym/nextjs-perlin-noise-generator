f464ea2d769c9760c11eb4965bb6bdf19c69a809
sudowoodo-keym <20496472+sudowoodo-keym@users.noreply.github.com>
2026-02-12 14:01:45 -0500

    added cube

--- perlin-noise-generator/app/components/drawingboard/Drawingboard.tsx
+++ perlin-noise-generator/app/components/drawingboard/Drawingboard.tsx
@@ -1,7 +1,53 @@
-import styles from "./drawingboard.module.css"
+"use client";
+
+import styles from "./drawingboard.module.css";
+import { useEffect, useRef } from "react";
 
 export const Drawingboard = () => {
+
+  const sketchRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    let instance: any;
+
+    const loadP5 = async () => {
+      const p5 = (await import ("p5")).default;
+
+      const sketch = (p: any) => {
+        p.setup = () => {
+          p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL);
+        };
+
+        p.draw = () => {
+          p.background(20);
+
+          const rotationX = p.map(p.mouseY, 0, p.height, -p.PI, p.PI);
+          const rotationY = p.map(p.mouseX, 0, p.width, -p.PI, p.PI);
+
+          p.rotateX(rotationX);
+          p.rotateY(rotationY);
+
+          p.normalMaterial();
+          p.box(150);
+        };
+
+        p.windowResized = () => {
+          p.resizeCanvas(p.windowWidth, p.windowHeight);
+        };
+      };
+
+      if (sketchRef.current) {
+        instance = new p5(sketch, sketchRef.current);
+      }
+    };
+
+    loadP5();
+
+    return () => {
+      instance?.remove(); // cleanup on unmount
+    };
+  }, []);
   return (
-    <div></div>
+    <div ref={sketchRef}/>
   )
 }
