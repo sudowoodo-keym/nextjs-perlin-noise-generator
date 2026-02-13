756e3226ee82ecfd7d0b882ddc1ffec7dd3a816b
keym <20496472+sudowoodo-keym@users.noreply.github.com>
2026-02-12 19:44:27 -0500

    added grid

---
+++ perlin-noise-generator/app/components/infiniteterrain/InfiniteTerrain.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+
+export const InfiniteTerrain = () => {
+  
+  const sketchRef = useRef<HTMLDivElement>(null);
+
+  let instance: any;
+
+  let cols: any, rows: any;
+  let scl = 20;
+
+  
+  useEffect(() => {
+
+    const loadP5 = async () => {
+      const p5 = (await import ("p5")).default;
+
+      const sketch = (p: any) => {
+        p.setup = () => {
+          p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL); 
+          cols = Math.floor(p.windowWidth / scl);
+          rows = Math.floor(p.windowHeight / scl);
+        };
+       
+        p.draw = () => {
+          p.background(0);
+
+          p.translate(-p.width / 2, -p.height / 2);
+
+          for (let x = 0; x < cols; x++) {
+            for (let y = 0; y < rows; y++) {
+              p.stroke(255);
+              p.noFill();
+              p.rect(x*scl,y*scl,scl,scl)
+            }
+          }
+
+        }
+        // p.draw = () => {
+        //   p.background(0);
+        //
+        //   p.rotateX(p.frameCount * 0.01);
+        //   p.rotateY(p.frameCount * 0.01);
+        //
+        //   p.normalMaterial();
+        //   p.box(200);
+        // };
+      };
+
+      if (sketchRef.current) {
+        instance = new p5(sketch, sketchRef.current);
+      }
+    };
+
+    loadP5();
+
+    return () => {
+      instance?.remove(); // cleanup on unmount
+    };
+  }, []);
+
+  return (
+    <div ref={sketchRef}/>
+  )
+}
+
--- perlin-noise-generator/app/page.tsx
+++ perlin-noise-generator/app/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import styles from "./homepage.module.css";
 import { Navbar } from "./components/navbar/Navbar";
-import { Drawingboard } from "./components/drawingboard/Drawingboard";
+// import { Drawingboard } from "./components/drawingboard/Drawingboard";
+import { InfiniteTerrain } from "./components/infiniteterrain/InfiniteTerrain";
 
 export default function Home() {
   return (
@@ -11,9 +12,13 @@ export default function Home() {
         <div className={`${styles.hero} w-full h-svh text-center flex flex-col items-center justify-center bg-black px-12`}>
           <p className="text-3xl font-bold">Perlin Noise Generator</p>
         </div>
+        {/* <div className="w-full h-svh text-center flex flex-col items-center justify-center bg-black px-12"> */}
+        {/*   <Drawingboard /> */}
+        {/* </div> */}
         <div className="w-full h-svh text-center flex flex-col items-center justify-center bg-black px-12">
-          <Drawingboard />
+          <InfiniteTerrain />
         </div>
+
       </div>
     </div> 
   );
