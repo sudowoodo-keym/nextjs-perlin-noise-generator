73fe9838840d4d4b6ae2f1d1497d8c1b3393c2c5
keym <20496472+sudowoodo-keym@users.noreply.github.com>
2026-02-14 08:16:41 -0500

    Updated navbar

--- perlin-noise-generator/app/components/navbar/Navbar.tsx
+++ perlin-noise-generator/app/components/navbar/Navbar.tsx
@@ -1,12 +1,14 @@
 import styles from "./navbar.module.css"
+import Link from 'next/link';
 
 export const Navbar = () => {
   return (
     <div className="w-full h-20 md:h-16 bg-neutral-900 backdrop-blur-xl z-50 fixed">
       <div className="h-20 md:h-16 flex justify-between items-center px-12">
-        <p className="font-semibold mx-8">Home</p>
-        <p className="font-semibold mx-8">Perlin Noise Generator</p>
-        <p className="font-semibold mx-8">Iggo</p>
+        <p className="font-semibold mx-8 text-nowrap">Perlin Noise Generator</p>
+        <Link href="https://portfolio-iggo.vercel.app">
+          <p className="font-semibold mx-8">Iggo</p>
+        </Link>
       </div>
     </div>
   )
